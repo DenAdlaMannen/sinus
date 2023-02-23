f146c0a8fa97fc33e88ac1b813d575673881bde4
Emanuelskod <Backhax@hotmail.com>
2023-02-23 19:42:22 +0100

    Search update

--- search.php
+++ search.php
@@ -2,22 +2,31 @@
 
 
    
-    if(isset($_POST["search"]))
-    {
+
       $conn = Connection::Connection();
-      
-      if($conn){
+
         
         $items = $_POST["search"];
-        $query = "select * from products where Title Like ?;";
-        $run = $mysqli->prepare($conn, $query);
 
-        if($run == TRUE)
-        {
+        $query = "select Title, Color, Price, Image from products where Title Like ?;";
+        $stmt = $conn->prepare($query);
 
-        }
+        $like = "%" . $items . "%";
+        $stmt->bind_param("s", $like);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
+        // var_dump($result);
+      
+
+       while ($row = $result->fetch_assoc()) {
+          echo $row['Title'] . $row['Color'] . $row['Price'];
+          echo'<img height="300" width="300" src="'.$rows['Image'].'">';
       }
-    }
+
+      if(1 == 1){
+       echo "1 = 1";
+      }
+    
 
     echo "You did not search!";

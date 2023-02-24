82ce5fc14a7c69dd8c6bdde934bc46c94513ef3b
Emanuelskod <Backhax@hotmail.com>
2023-02-24 10:58:36 +0100

    Minor updates to index, menu, search and style

--- search.php
+++ search.php
@@ -8,25 +8,63 @@
         
         $items = $_POST["search"];
 
-        $query = "select Title, Color, Price, Image from products where Title Like ?;";
+        $query = "select Title, Color, Price, Image, ProductID from products where Title Like ?;";
         $stmt = $conn->prepare($query);
 
         $like = "%" . $items . "%";
         $stmt->bind_param("s", $like);
         $stmt->execute();
-        $result = $stmt->get_result();
 
+
+
+        $users = $stmt->get_result()->fetch_all(MYSQLI_ASSOC); 
         // var_dump($result);
       
 
-       while ($row = $result->fetch_assoc()) {
-          echo $row['Title'] . $row['Color'] . $row['Price'];
-          echo'<img height="300" width="300" src="'.$rows['Image'].'">';
-      }
+       foreach ($users as $row) 
+        
+        {
+          // echo $row['Title'] . "<br>" . $row['Color'] . "<br>" .$row['Price'];
+          // echo' <img src="data:image/png;base64,'.base64_encode($row['Image']).'" >';
+          // echo "<br>";
+      
 
-      if(1 == 1){
-       echo "1 = 1";
-      }
+      ?>
+
+      
     
 
-    echo "You did not search!";
+
+
+        <style> 
+        .productFlexBox {
+            display: flex;
+            justify-content: center;
+            flex-flow: row-reverse wrap;
+            background-color: white;
+        } 
+        .card {
+            margin: 2em;
+            border: none;
+        }
+        </style>
+
+
+      <div class="card" style="width: 18rem;">
+        <img src="sinusmaterial/sinus assets/products/hoodie-fire.png" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h4 class="card-title"><?php echo $row["Title"]; ?> </h4>
+            <hr>
+            <h6 class="card-title">Color:<?php echo $row["Color"];?></h6>
+            <h6 class="card-title"><?php echo $row["Price"]; ?> kr</h6>
+             
+             <form method="POST" action="productInfo.php"> 
+                <input type="submit" name="Details">
+                <input type="hidden" name="id" value="<?php echo $row['ProductID']; ?>"/>
+            </form>
+        </div>
+
+      </div>
+<?php
+}
+?>

64644650a695da5169743a3194e0b53579e5e63d
ToBeOtt <119058608+ToBeOtt@users.noreply.github.com>
2023-02-25 10:55:07 +0100

    Minor changes
    Updated new buttons and added include on confirmcart

--- confirmCart.php
+++ confirmCart.php
@@ -6,31 +6,17 @@ if (isset($_POST['id'])) {
     array_push($_SESSION['Cart'], $_POST['id']);
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Sinus</title>
-  <link rel="stylesheet" href="style.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 <style>
-    div { 
-        text-align: center;
-        vertical-align: center;
-    }
-    
+    .card { border: none; margin: 5em;}
+    .card-header { background: white;}
 </style>
-
-</head>
-<body>
-    <div>
-            <h4 class="card-title">Your item has been added to your cart!</h4>
-            <hr>
-            <a href="index.php">Return</a>
-    </div>
-
-</body>
-</html>
+<div class="card"> 
+  <div class="card-header" align="center">
+  <h1 class="display-4">Cart updated!</h1>
+  </div>
+  <div class="card-body" align="center">
+    <h5 class="card-title" align="center">Your item has been added to your cart!</h5>
+    <p class="card-text" align="center">What do you want to do next?</p>
+    <a href="index.php" align="center" class="btn btn-primary">Return to shopping</a>
+  </div>
+</div>
--- productInfo.php
+++ productInfo.php
@@ -1,5 +1,4 @@
-<?php require 'connection.php';
-?>
+<?php require 'connection.php';?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,7 +10,6 @@
   <link rel="stylesheet" href="style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-
 <style>
     .productView {text-align:center;}
     .card-img-full {
@@ -23,16 +21,15 @@
 </style>
 </head>
 <body>
-
-    <!-- CONTAINER START -->
-    <div class="container">
+<!-- CONTAINER START -->
+<div class="container">
 
   <!-- HEADER START -->
 <?php require 'menu.php'?>
 
 <!-- MAIN START -->
 <div class="main">
-<?php if(isset($_POST['id'])) {
+<?php if(isset($_POST['id']) && empty($_POST['addtocart'])) {
 ViewProduct($_POST['id']); ?>
 <?php }  
 ?>
@@ -65,14 +62,21 @@ ViewProduct($_POST['id']); ?>
             <h4>Price: <?php echo $row['Price'];?></h4>
             <h4>Category: <?php echo $row['CategoryName'];?></h4>
             <img src="sinusmaterial/sinus assets/products/<?php echo $row["Image"]?>" class="card-img-full" alt="...">
-            <form method="POST" action="confirmCart.php"> 
-                <input type="submit" class="btn btn-outline-secondary" value="Add to cart" >
+            <form method="POST"> 
+                <input type="submit" name ="addtocart" class="btn btn-outline-secondary" value="Add to cart" >
                 <input type="hidden" name="id" value="<?php echo $row["ProductID"]; ?>"/>
             </form>
         </div>
 <?php      }
     } 
 ?>
+
+<!-- INCLUDES ADD TO CART IF PRESSED -->
+<?php if(isset($_POST["addtocart"]))
+{
+    include "confirmCart.php";
+} ?>
+
 <div> 
 </div>
 </div>

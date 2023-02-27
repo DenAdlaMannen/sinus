5142375aaa203af5ecde618c887c9c6297e509e1
Julia <derpette87@gmail.com>
2023-02-27 23:52:45 +0100

    Checkout: create customer, order and orderline.
    FINALLY!

--- checkout2.php
+++ checkout2.php
@@ -1,100 +1,85 @@
 <!DOCTYPE html>
 <html>
-
+<body>
 
 <body>
 
-
-
+<p>You have chosen the follwing items:</p>
+<br>
 <?php require_once 'connection.php';?>
 <?php session_start(); ?>
-
-<?php require_once 'connection.php';?>
-<?php session_start(); ?>
-<h3>You have chosen the follwing items:</h3>
 
 <?php
-function getItemsInCartList($sessionCart) {
-    $itemsInCartList = array();
-    
 
-    if (!empty($sessionCart)) {
-        $unpackCart = implode(',', $sessionCart);
+    $itemsInCartList = 0;
+    $orderLineLength = 0;
 
-        $conn = Connection::Connection();
-        $calcTotalPrice = 0;
-
-        foreach ($sessionCart as $item) {
-            $stmt = $conn->prepare("SELECT *
+    if(isset($_POST["sessionCart"]))
+    {
+        if(!empty($_SESSION['Cart'])) {
+            $unpackCart = implode(',', $_SESSION['Cart']);
+        
+            $conn = Connection::Connection();
+            $calcTotalPrice = 0;
+        
+            foreach ($_SESSION['Cart'] as $item) {
+                $stmt = $conn->prepare("SELECT *
                 FROM Products
                 WHERE ProductID=?;");
-
+        
             $stmt->bind_param("s", $item);
-
+        
             $stmt->execute();
-
+        
             $result = $stmt->get_result();
-
-            while ($row = $result->fetch_assoc()) {
-                $itemsInCartList[] = $row; // saves query result as array
-                $calcTotalPrice = $calcTotalPrice + $row['Price'];
-                echo  "<br>" . $row["Title"].  " " . $row['Color'] .  " ". $row['Price']. " SEK" . "<br>";
-
+        
+            while($row = $result->fetch_assoc()) { 
+               // $itemsInCartList[]=$row; //collects all search results in array
+                ?>
+            <tr>
+            <th scope="row">1</th>
+            <td><?php echo $row['Title'];?></td>
+            <td><?php echo $row['Color'];?></td>
+            <td><?php echo $row['Price'];?> kr</td>
+            </tr> 
+        <?php  $calcTotalPrice = $calcTotalPrice + $row['Price'];
             }
-            
-        }
-    }echo "<h4>total price for order:   $calcTotalPrice SEK</h4>";
 
-    return $itemsInCartList;
-}
+        } 
+        }
+        /*echo "<br>";
+        print_r($itemsInCartList);*/
+     
+
+    }
+
 ?>
 
 <?php
-$itemsInCartList = getItemsInCartList($_SESSION['Cart']);
-$orderLineLength = sizeof($itemsInCartList);
-
-//var_dump($orderLineLength);
-
-
-/*echo "<pre>";
-echo print_r($itemsInCartList);
-echo "</pre>";*/
-
-//print_r($itemsInCartList);
 
 $firstName = $lastName = $country = $city = $zipcode = $street = $phone = $email = "";
 
 if ($_SERVER["REQUEST_METHOD"] ==  "POST") {
 
-
-    if (isset($_POST['firstname'])) {
-        $firstName = test_input($_POST["firstname"]);
-    }
-    if (isset($_POST['lastname'])) {
-        $lastName = test_input($_POST["lastname"]);
-    }
-    if (isset($_POST['country'])) {
-        $country = test_input($_POST["country"]);
-    }
-    if (isset($_POST['city'])) {
-        $city = test_input($_POST["city"]);
-    }
-    if (isset($_POST['zipcode'])) {
-        $zipcode = test_input($_POST["zipcode"]);
-    }
-    if (isset($_POST['street'])) {
-        $street = test_input($_POST["street"]);
-    }
-    if (isset($_POST['city'])) {
-        $city = test_input($_POST["city"]);
-    }
-    if (isset($_POST['tel'])) {
-        $phone = test_input($_POST["tel"]);
-    }
-    if (isset($_POST['email'])) {
-        $email = test_input($_POST["email"]);
-    }
-
+    if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === "POST")
+ {
+    $firstName = filter_input(INPUT_POST, 'firstname', FILTER_UNSAFE_RAW);
+    $action = filter_input(INPUT_POST, 'lastname', FILTER_UNSAFE_RAW);
+    echo "<br> Nu är det postat! <br>";
+ } 
+ else
+ {
+    exit;
+ }
+    $firstName = test_input($_POST["firstname"]);
+    $lastName = test_input($_POST["lastname"]);
+    $country = test_input($_POST["country"]);
+    $city = test_input($_POST["city"]);
+    $zipcode = test_input($_POST["zipcode"]);
+    $street = test_input($_POST["street"]);
+    $city = test_input($_POST["city"]);
+    $phone = test_input($_POST["tel"]);
+    $email = test_input($_POST["email"]);
     
 }
 
@@ -160,39 +145,50 @@ function test_input($data){
         <br>
         Card Number: <input type="text" name="namn" required>
         <br>
-        CVV Numbers <input type="text" name="cvv" required>
+        CVS Numbers <input type="text" name="cvs" required>
         <br>
         <br>
         <button>Submit Order</button>
     </form>
 </fieldset>
-
+</form>
 
 <?php
+
 include_once "customer.php";
 require_once "connection.php";
 include_once "orderFunctions.php";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST["firstname"]) && !empty($_POST["lastname"]) && !empty($_POST["country"]) && !empty($_POST["city"]) && !empty($_POST["zipcode"]) && !empty($_POST["street"]) && !empty($_POST["tel"]) && !empty($_POST["email"]) )
-
-{
 
 
 $newCustomer = New Customer ($_POST["firstname"], $_POST["lastname"],$_POST["country"],$_POST["city"],$_POST["zipcode"],$_POST["street"],$_POST["tel"],$_POST["email"]);
 
+
 $conn = Connection::Connection();
-$customerID = Customer::InsertCustomerDB($conn, $newCustomer); //creates a customer in db
-$order_ID = OrderFunctions::CreateOrder($customerID); //creates an order in db, linking customer
-OrderFunctions::CreateOrderLine($order_ID, $orderLineLength, $itemsInCartList);
+$customerID = Customer::InsertCustomerDB($conn, $newCustomer); //creates a new customer in db and returns  id
+//$order_ID = OrderFunctions::CreateOrder($customerID); // creates a new order in db and returns id
+//OrderFunctions::CreateOrderLine($order_ID,$orderLineLength, $itemsInCartList);
 
-//echo "THIS IS ORDER ID: " . $order_ID;
+echo print_r($itemsInCartList);
+echo "</pre>";
+var_dump($customerID);
+echo "<br>";
+echo "here is order_ID ";
+var_dump($order_ID);
 
-//var_dump($newCustomer);
-}
+    
+
+
+
 ?>
+<?php if (count($_POST)>0) echo '<h1>Thank you for shopping at Sinus!</h1>'; 
 
+session_unset();
 
-<?php if (count($_POST)>0) echo '<h1>Thank you for shopping at Sinus!</h1>'; ?>
+// destroy the session
+session_destroy();
+
+?>
 
 </body>
 </html>
--- orderFunctions.php
+++ orderFunctions.php
@@ -1,5 +1,6 @@
 <?php
  require_once "connection.php";
+ require_once "checkout.php";
  
 class OrderFunctions
 {
@@ -15,33 +16,17 @@ class OrderFunctions
         $nowDate = date("Y/m/d H:i:s");
 
         $stmt->execute();
-        $order_ID = $conn->insert_id; //snaking variable name so it doesn't collide with the table row "OrderID"
+        $order_ID = $conn->insert_id;
+
+        return $order_ID;
 
     }
 
     public static function CreateOrderLine ($order_ID, $orderLineLength, $itemsInCartList)
     {
-        $conn = Connection::Connection();
+       $conn = Connection::Connection();
 
-        echo $itemsInCartList['ProductID'][0];
-
-        foreach ($itemsInCartList as $item)
-        {
-            if (!empty($item))
-            {
-                $stmt = $conn->prepare("INSERT INTO orderline (ProductID, OrderID, Quantity, TotalPrice) VALUES (?,?,?,?)");
-        $stmt ->bind_param("iiii",$idProduct,$order_ID,$quantity,$totalprice);
-
-        $idProduct = $itemsInCartList[$item]['ProductID'];
-        $quantity = 1;
-        $totalprice = $itemsInCartList[$item]['Price'];
-
-        $stmt->execute();
-            }
-
-        }
-
-       /* for ($i = 0; $i<$orderLineLength; $i++)
+       for ($i = 0; $i<$orderLineLength; $i++)
         {
         
         $stmt = $conn->prepare("INSERT INTO orderline (ProductID, OrderID, Quantity, TotalPrice) VALUES (?,?,?,?)");
@@ -52,28 +37,11 @@ class OrderFunctions
         $totalprice = $itemsInCartList[$i]['Price'];
 
         $stmt->execute();
-        }*/
+        }
         
 
     }
 
-    public static function CreateOrderLine2 ($order_ID,$itemsInCartList)
-    {
-        $conn = Connection::Connection();
-
-        
-        $stmt = $conn->prepare("INSERT INTO orderline (ProductID, OrderID, Quantity, TotalPrice) VALUES (?,?,?,?)");
-        $stmt ->bind_param("iiii",$idProduct,$order_ID,$quantity,$totalprice);
-
-        $idProduct = $itemsInCartList[0]['ProductID'];
-        $quantity = 1;
-        $totalprice = $itemsInCartList[0]['Price'];
-        
-        $stmt->execute();
-        
-        
-
-    }
   
 
 }

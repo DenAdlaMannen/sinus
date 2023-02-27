b2b3be0e27231aade144465ff5ca426fa85d6b04
ToBeOtt <119058608+ToBeOtt@users.noreply.github.com>
2023-02-27 13:53:01 +0100

    Made a change for UI but it has not been merged yet

--- adminNav.php
+++ adminNav.php
@@ -1,16 +1,17 @@
-<style>
-  .adminImg {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  display: inline-block;
-  Vertical-align: top;}
-</style>
-
-<!-- THIS IS THE BAR ON TOP WHERE THE SVG FOR ADMIN IS LOCATED. -->
-
 <nav class="navbar navbar-light bg-white">
-  <a class="navbar-brand" href="indexAdmin.php">
-    <img src="sinusmaterial/sinus assets/logo/person-gear.svg" width="40" height="40" class="adminImg" alt="">
-  </a>
+<table>
+    <tr>
+    <th>
+        <a class="navbar-brand" href="indexAdmin.php">
+            <img src="sinusmaterial/sinus assets/logo/person-gear.svg" width="40" height="40" class="adminImg" alt="Small image of a torso and head with a small gearhead on it.">
+        </a>
+    </th>
+    <th class="empty"></th> <style> .empty { width: 90vw;} </style>
+    <th>
+        <a class="navbar-brand" href="indexAdmin.php">
+            <img src="sinusmaterial/sinus assets/logo/cart4.svg" width="40" height="40" class="adminImg" alt="Small image of a torso and head with a small gearhead on it.">
+        </a>
+    </th>
+  </tr>
+</table>
 </nav>
